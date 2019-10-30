64b86c5a11ec67b351dbac378c90a607b4b0c138
kyu9341 <kyu9341@naver.com>
2019-10-31 01:09:30 +0900

    이미지 url로 출력 구현 검색 오류 해결

--- Django/MovieRecommendation/moviecloud/views.py
+++ Django/MovieRecommendation/moviecloud/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import MovieData
-from django.urls import reverse
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request): # 홈페이지
     if request.method == 'GET': # get방식으로 접근 시
-        return render(request, 'moviecloud/home.html') # 홈페이지 출력
+        all_moviedata = MovieData.objects.all().order_by('id')
+        page = request.GET.get('p', 1) # 페이지를 받아옴
+        paginator = Paginator(all_moviedata, 10) # 한번에 보여줄 개수
+        moviedata = paginator.get_page(page)
+
+        return render(request, 'moviecloud/home.html', {'moviedata' : moviedata}) # 홈페이지 출력
     elif request.method == 'POST': # 영화 제목을 입력 받은 경우
         title = request.POST.get('title', None)  # 템플릿에서 입력한 name필드에 있는 값을 키값으로 받아옴
-        res_data = {}  # 응답 메세지를 담을 변수(딕셔너리)
         #movieDetail = MovieData.objects.get(title=title)
-        movieDetail = MovieData.objects.all() # 전체 목록을 가져옴
-        cnt = 0
-        for row in movieDetail:
-            cnt += 1
-            if row == title:
-                break
-        pk = cnt # 제목과 일치하는 pk(primary key) 가져옴
-        return redirect('/moviecloud/detail/'+str(pk)) # url로 redirection
+
+        try:
+            movieDetail = MovieData.objects.get(title__startswith = title) # 전체 목록을 가져옴
+        except MovieData.DoesNotExist:
+            return HttpResponse("등록되지 않은 영화입니다.")
+
+        return redirect('/moviecloud/detail/'+str(movieDetail.pk)) # url로 redirection
 
 
 def detail(request, pk): # 영화 상세보기 페이지
@@ -32,10 +35,17 @@ def detail(request, pk): # 영화 상세보기 페이지
 
 def inputdata(request): # 크롤링해서 가공된 데이터를 자동화하여 데이터베이스에 입력하기 위한 함수
     if request.method == 'GET':
+        mlist = list()
+        mf = open('../../crawling/movie/movie_list.txt')
+        while True:
+            mline = mf.readline()
+            if not mline:
+                break
+            mlist.append(mline.replace('\n', '').replace(':', '')) # \n 제거 (문자열 정제)
 
-        for i in range(1, 9):
+        for i in mlist:
             lineNum = 1
-            f = open('moviedata/'+str(i) + '.txt', 'r',  encoding="utf8")
+            f = open('../../crawling/movie/'+i+'/'+i+ '.txt', 'r',  encoding="utf8")
             while True:
                 line = f.readline() # 각 파일을 줄 단위로 읽어옴
                 if not line:
@@ -54,7 +64,8 @@ def inputdata(request): # 크롤링해서 가공된 데이터를 자동화하여
                     premier = line
                 if lineNum == 7: # 관객 별점
                     score = line
-                    wordcloud = 'https://raw.githubusercontent.com/kyu9341/Python-Movie-Recommendation/master/Django/MovieRecommendation/uploads/wordclouds/'+title+'_wordcloud.png'
+                    wordcloud = 'https://raw.githubusercontent.com/kyu9341/Python-Movie-Recommendation/master/crawling/movie/'+title+'/'+title+'_wordcloud.png'
+
                 lineNum += 1 # 라인 넘버 ++
 
             moviedata = MovieData( # 각 데이터들을 데이터베이스의 하나의 튜플로 저장
